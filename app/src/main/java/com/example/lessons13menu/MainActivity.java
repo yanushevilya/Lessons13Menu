6fd5691de5434b2832a8089129bd7a88b5fdcb70
yanushev <yanushev.ilya@gmail.com>
2021-06-15 16:06:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lessons13menu/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lessons13menu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // ================ MENU begin ================
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("File");
+        menu.add("View");
+        menu.add("Options");
+        menu.add("About");
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+    // ================ MENU end ================
+
+}
